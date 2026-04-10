28124e91f467fb6f6b5ea5061dd6d2db74814869
local <local@levelup.local>
2026-04-10 17:13:18 +0200

    session 1: clone homepage wrapped in Astro Layout/Nav/Footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
   site: 'https://welevelup.org',
+  trailingSlash: 'never',
+  build: {
+    format: 'file',
+  },
+  integrations: [sitemap()],
 });

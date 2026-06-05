b882b33f0386a880903270aa4a6d1e4363554f0d
crojasu <catalina@welevelup.org>
2026-06-05 15:54:56 +0300

    Revert "fix: use @astrojs/vercel/serverless mode to properly serve API routes"
    This reverts commit 11a1e5bf8a2aa78813b4b7ebf1dc76474b37baa0.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // build: 2026-05-27
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
